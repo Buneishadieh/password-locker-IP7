562b8af7f6a97531f9cc13cb2c0f68a4859e7f63
munene19 <munenexv@gmail.com>
2019-12-16 05:14:54 +0300

    added create user function on run file

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/Desktop/password_locker/env python3.7
+from password_locker import User
+from password_locker import Credentials
+
+
+def create_user(user_name, password, email):
+    # Function to create a new user.
+
+    new_user = User(user_name, password, email)
+    return new_user
